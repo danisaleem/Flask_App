0df9fc7584bb5cdeaf26f867aa81c54dc31e384b
Daniyal Saleem <daniyalsaleem.d@gmail.com>
2020-04-07 15:15:41 +0200

    first commit my flask app

---
+++ migrations/versions/492842fc3ad0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 492842fc3ad0
+Revises: 38a1ba2ccd05, 780739b227a7
+Create Date: 2020-03-26 16:09:41.591941
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '492842fc3ad0'
+down_revision = ('38a1ba2ccd05', '780739b227a7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

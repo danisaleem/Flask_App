55c5df528e0759cc674501a8323257cdcc1b17c3
Daniyal Saleem <daniyalsaleem.d@gmail.com>
2020-04-08 00:01:45 +0200

    email-support reset password

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ class Config(object):
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587) # 25)
     MAIL_USE_TLS = 1 #os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'your_email_id@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'Your_password'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'password'
     ADMINS = ['victorion_d@yahoo.com']
     POSTS_PER_PAGE = 3
